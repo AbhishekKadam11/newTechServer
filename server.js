a851667dbbae36bbc6a76a63314c42dc0ad334a4
demoemailid1101 <demo.emailid1101@gmail.com>
2017-09-27 20:29:37 +0530

    Changes in mongooes connection.

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.use(express.static(__dirname + '/'));
 app.use(passport.initialize());
 
 // Start server
-var port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var port = process.env.PORT || 8080
   , ip = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
 app.listen(port, ip, function () {
   console.log('Express server listening on %d', port);
