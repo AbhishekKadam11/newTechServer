26c879bd726eab7e546cd3be8a96ad4178ced833
demoemailid1101 <demo.emailid1101@gmail.com>
2017-09-30 18:29:24 +0530

    Check live mongodb connection

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 
 module.exports = {
   'secret': 'AbhishekIsAwesome',
-  'database': process.env.MONGODB_URI || 'mongodb://localhost/newTech'
+  'database': process.env.MONGODB_URI2 || 'mongodb://localhost/newTech'
 };
